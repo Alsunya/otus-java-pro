5e0bc80f2d4e080a1cc6a7827dd5649780ec1c26
Alsu <alsu1408@yandex.ru>
2024-04-15 22:32:02 +0300

    Практика Stream API

---
+++ StreamAPI/src/main/java/org/tasks/App.java
@@ -0,0 +1,25 @@
+package org.tasks;
+
+import static org.tasks.TaskAnalyser.*;
+import static org.tasks.TaskAnalyser.getTasksCountByStatus;
+
+/**
+ * TЗ:
+ * Имеется список объектов класса Задача (ID, название, статус). С помощью Stream API реализуйте:
+ * Получение списка задач по выбранному статусу;
+ * Проверка наличия задачи с указанным ID;
+ * Получение списка задач в отсортированном по статусу виде: открыта, в работе, закрыта (можете выбирать любой статус и любой порядок, главное чтобы было 3 разных статуса);
+ * Подсчет количества задач по определенному статусу.
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(getTasksByStatus(Status.NEW).toString());
+        System.out.println(isTaskExist(3));
+        System.out.println(isTaskExist(10));
+        System.out.println(getSortedTasksByStatus().toString());
+        System.out.println(getTasksCountByStatus(Status.NEW));
+        System.out.println(getTasksCountByStatus(Status.IN_PROGRESS));
+    }
+}
---
+++ StreamAPI/src/main/java/org/tasks/TaskAnalyser.java
@@ -0,0 +1,41 @@
+package org.tasks;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TaskAnalyser extends CommonTask {
+
+    /**
+     * Получение списка задач по выбранному статусу;
+     */
+    public static List<Task> getTasksByStatus(Status status) {
+        return Task.getTasksStream().filter(task -> task.getStatus().equals(status))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Проверка наличия задачи с указанным ID;
+     */
+    public static boolean isTaskExist(int id) {
+        return Task.getTasksStream().anyMatch(task -> task.getId() == id);
+    }
+
+    /**
+     * Получение списка задач в отсортированном по статусу виде:
+     * открыта, в работе, закрыта (можете выбирать любой статус и любой порядок, главное чтобы было 3 разных статуса);
+     */
+    public static List<Task> getSortedTasksByStatus(){
+        return Task.getTasksStream().sorted(Comparator.comparing(Task::getStatus))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Подсчет количества задач по определенному статусу.
+     */
+    public static long getTasksCountByStatus(Status status){
+        return Task.getTasksStream().filter(task -> task.getStatus().equals(status))
+                .count();
+
+    }
+}
