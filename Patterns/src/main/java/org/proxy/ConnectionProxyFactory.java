71b47ecc5e8e4e21ce12caf1976dadf749cee302
Alsu <alsu1408@yandex.ru>
2024-09-18 01:12:30 +0300

    Удалила неактуальное

--- Patterns/src/main/java/org/proxy/ConnectionProxyFactory.java
+++
@@ -1,9 +0,0 @@
-package org.proxy;
-
-import java.sql.Connection;
-
-public class ConnectionProxyFactory {
-    public static ConnectionProxy createProxy(Connection connection, boolean autoCommit) {
-        return new ConnectionProxyImpl(connection, autoCommit);
-    }
-}
